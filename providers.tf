b36cc5b9826d998f6726a465903c15581600f169
Tim Grant <tim.grant@appvia.io>
2023-11-02 10:11:23 +0000

    Use OIDC

--- providers.tf
+++ providers.tf
@@ -1,16 +1,8 @@
 provider "azurerm" {
-  use_msi = true
-  subscription_id = "c37a3080-9ea8-4fc6-8b55-cf949c497a58"
-  tenant_id = "7a770e35-b455-4df2-a276-b07408438d9a"
-  client_id = "f3072f15-353e-4642-b4a8-fff3433b0be5"
   features {}
 }
 
 provider "azapi" {
-  use_msi = true
-  subscription_id = "c37a3080-9ea8-4fc6-8b55-cf949c497a58"
-  tenant_id = "7a770e35-b455-4df2-a276-b07408438d9a"
-  client_id = "f3072f15-353e-4642-b4a8-fff3433b0be5"
 }
 
 provider "helm" {
